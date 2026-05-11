43e38bbfb8550436ad836a1002ab2e35563d7ac0
phil-macrocheira <philip.underwood2@gmail.com>
2026-05-10 19:06:59 -0500

    Log file max size

--- UFO 50 Mod Loader/Services/LogService.cs
+++ UFO 50 Mod Loader/Services/LogService.cs
@@ -32,10 +32,36 @@ public static class LogService
         try {
             if (string.IsNullOrWhiteSpace(MainLog))
                 return;
+
             File.AppendAllText(Constants.LogPath, MainLog);
+
+            // Trim log file if exceeds 10mb
+            if (LogFileIsMaxSize(10 * 1024 * 1024)) {
+                TrimLogFile();
+            }
         }
         catch (Exception ex) {
             OnLog?.Invoke($"Failed to save log to file: {ex.Message}");
         }
+    }
+    private static bool LogFileIsMaxSize(long maxBytes)
+    {
+        if (File.Exists(Constants.LogPath)) {
+            var info = new FileInfo(Constants.LogPath);
+            if (info.Length >= maxBytes)
+                return true;
+            else
+                return false;
+        }
+        return false;
+    }
+    private static void TrimLogFile()
+    {
+        var content = File.ReadAllText(Constants.LogPath);
+        var trimmed = content.Substring(content.Length / 2);
+        var newlineIndex = trimmed.IndexOf('\n');
+        if (newlineIndex >= 0)
+            trimmed = trimmed.Substring(newlineIndex + 1);
+        File.WriteAllText(Constants.LogPath, trimmed);
     }
 }
